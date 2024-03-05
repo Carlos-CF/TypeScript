2c74ff087c1881a03fbeaac3bf3a8c60043f71fd
Carlos Fernandes <carloscaetanofernandes@hotmail.com>
2024-03-04 22:13:03 -0300

    Continuação da aula de nodejs 04/03/24

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,13 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response)=> {
-    return res.json({nome: 'Carlos'});
+   //throw new Error('Erro ao fazer requisção');
+   return res.json({nome: 'Carlos'})
 })
+
+/*
+router.get('/teste', (req: Request, res: Response)=> {
+    return res.json({nome: 'Carlos'});
+})*/
 
 export {router};
